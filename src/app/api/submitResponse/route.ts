0fe08711b16ba41a1af4f759e7c88eb4b455f76d
Samuel <28797947+sambensim@users.noreply.github.com>
2024-09-29 21:20:51 -0700

    Hopefully Improved Error Logs in POST

--- src/app/api/submitResponse/route.ts
+++ src/app/api/submitResponse/route.ts
@@ -4,6 +4,9 @@ import { openDB } from '../../../lib/db';
 export async function POST(request: Request) {
     try {
         const { word1_id, word2_id, selected_id } = await request.json();
+        if (!word1_id || !word2_id || selected_id) {
+            throw new Error("Missing fields in the request body");
+        }
         const db = await openDB();
 
         await db.run(
@@ -14,6 +17,7 @@ export async function POST(request: Request) {
         );
         return NextResponse.json({ message: 'Response recorded' });
     } catch (error) {
+        console.error('Error in POST:\n' + error);
         return NextResponse.json({ error: 'Failed to record response:\n' + error }, { status: 500 })
     }
 }
